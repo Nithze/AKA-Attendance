2cfc1dfd07757932095f72e4408f65bcbc9640aa
Nithze <aldiyusronzhar@gmail.com>
2024-09-29 16:28:07 +0700

    add info card

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -14,7 +14,20 @@ export const Dashboard = () => {
                         <i class='bx bx-log-out'></i>
                     </div>
                 </div>
+                {/* navbar end */}
                 <hr></hr>
+
+                <div className="profile">
+                    <div className="info">
+                        <div className="name">Aldi Yusronazhar</div>
+                        <div className="nik">Morning Shift</div>
+                    </div>
+                    <div className="role">
+                        <div className="role">Barista</div>
+                        <div className="salary">Salary Rp 3.000.000</div>
+                    </div>
+                </div>
+
             </div>
         </>
     )
